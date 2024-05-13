68c7a0c0d7aabdd69a38f6e2e8d2daabec00bbf2
putuangga16 <165106828+putuangga16@users.noreply.github.com>
2024-05-13 13:43:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const navbar = document.getElementsByTagName('nav')[0];
 window.addEventListener('scroll', function() {
     console.log(window.scrollY);
     if (window.scrollY > 1) {
-        navbar.classList.replace('bg-transparent', 'nav-color');
+        navbar.classList.replace('bg-dark', 'nav-color');
     } else if (window.scrollY <= 0) {
-        navbar.classList.replace('nav-color', 'bg-transparent');
+        navbar.classList.replace('nav-color', 'bg-dark');
     }
 
 });
